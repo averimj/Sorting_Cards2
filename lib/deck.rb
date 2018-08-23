c9d7a5d261b1bd23d80841db0ca38cb6cb96fad5
Averi Johnson <averimj@live.com>
2018-08-22 18:30:30 -0600

    Add bubble_sort method

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,20 @@ class Deck
   def count
     @cards.count
   end
+
+  def bubble_sort(array)
+    loop do
+      swap = false
+      (count - 1).times do |index|
+        if array[index].total_card_value > array[index + 1].total_card_value
+          array[index], array[index + 1] = array[index + 1], array[index]
+          swap = true
+        end
+      end
+      break if not swap
+    end
+    array
+  end
+
+
 end
